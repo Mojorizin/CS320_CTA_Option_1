36d36c4a10db1105c76836792466813485991773
Michael Masters <mastersm89@gmail.com>
2023-08-16 20:54:27 -0600

    My first commit of a Java application.

---
+++ src/fictional_person.java
@@ -0,0 +1,20 @@
+
+public class fictional_person 
+	{
+		public static void main(String[] args)
+		
+		{
+			String firstName = "Harry";
+			String lastName = "Potter";
+			String streetAddress = "4 Privet Dr.";
+			String city = "Little Whinning";
+			int zipCode= 12345;
+			
+			System.out.println(firstName);
+			System.out.println(lastName);
+			System.out.println(streetAddress);
+			System.out.println(city);
+			System.out.println(zipCode);
+		}
+
+}
